d33015ab80c1b17845d41853ceb9d85e7d5ffe83
Travis Wilson <35748617+trrwilson@users.noreply.github.com>
2024-02-18 02:24:19 -0800

    legacy completion protocol client

---
+++ OpenAI.Official/src/Custom/LegacyCompletions/LegacyCompletionClientOptions.cs
@@ -0,0 +1,8 @@
+namespace OpenAI.Official.LegacyCompletions;
+
+/// <summary>
+/// Client-level options for OpenAI legacy completion operations.
+/// </summary>
+public partial class LegacyCompletionClientOptions : OpenAIClientOptions
+{
+}
---
+++ OpenAI.Official/tests/TestScenarios/LegacyCompletions.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenAI.Official.Chat;
+using OpenAI.Official.LegacyCompletions;
+using System;
+using System.ClientModel;
+using System.ClientModel.Primitives;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace OpenAI.Official.Tests.LegacyCompletions;
+
+public partial class LegacyCompletionTests
+{
+    [Test]
+    public void BasicValidationWorks()
+    {
+        LegacyCompletionClient client = new();
+        BinaryData requestData = BinaryData.FromObjectAsJson(new
+        {
+            model = "gpt-3.5-turbo-instruct",
+            prompt = "hello world",
+            max_tokens = 256,
+            temperature = 0,
+        });
+        RequestBody content = RequestBody.CreateFromStream(requestData.ToStream());
+        Result result = client.GenerateLegacyCompletions(content);
+        Assert.That(result, Is.Not.Null);
+        JsonObject responseObject = JsonSerializer.Deserialize<JsonObject>(result.GetRawResponse().Content.ToString());
+        string text = responseObject["choices"].AsArray()[0].AsObject()["text"].ToString();
+        Assert.That(text, Is.Not.Null.Or.Empty);
+    }
+}
