c1cf612892cb0e566d3851344a2377606fd8a3fd
Chris Schraer <chris_schraer@users.noreply.github.com>
2024-08-07 10:43:45 -0700

    added test for invalid model

--- .dotnet/tests/FineTuning/FineTuningClientTests.cs
+++ .dotnet/tests/FineTuning/FineTuningClientTests.cs
@@ -9,57 +9,63 @@ using System.Threading.Tasks;
 
 namespace OpenAI.Tests.FineTuning
 {
-	[TestFixture]
-	public class FineTuningClientTests
-	{
-		[Test]
-		public void ClientDefaults()
-		{
-			var client = new FineTuningClient();
-			Assert.IsNotNull(client);
-		}
-
-		[Test]
-		public void ExceptionThrownOnInvalidFilename()
-		{
-			var client = new FineTuningClient();
-
-			// Wrong filename will respond with http 400
-			Assert.Throws<ClientResultException>(() =>
-				client.CreateJob(training_file: "Invalid File Name", model: "gpt-3.5-turbo")
-			);
+    [TestFixture]
+    public class FineTuningClientTests
+    {
+        [Test]
+        public void ClientDefaults()
+        {
+            var client = new FineTuningClient();
+            Assert.IsNotNull(client);
         }
 
         [Test]
-		public void CreateAndCancelJob()
-		{
+        public void ExceptionThrownOnInvalidFilename()
+        {
             var client = new FineTuningClient();
 
-			var fileClient = new FileClient();
-			string path = Path.Combine("Assets", "fine_tuning_sample.jsonl");
+            // Wrong filename will respond with http 400
+            Assert.Throws<ClientResultException>(() =>
+                client.CreateJob(training_file: "Invalid File Name", model: "gpt-3.5-turbo")
+            );
+            Assert.Throws<ClientResultException>(() =>
+            {
+                string path = Path.Combine("Assets", "fine_tuning_sample.jsonl");
+                client.CreateJob(training_file: path, model: "gpt-nonexistent");
+            }
+            );
+        }
 
-			OpenAIFileInfo file = fileClient.UploadFile(path, "fine-tune");
+        [Test]
+        public void CreateAndCancelJob()
+        {
+            var client = new FineTuningClient();
 
-			try
-			{
+            var fileClient = new FileClient();
+            string path = Path.Combine("Assets", "fine_tuning_sample.jsonl");
 
-				Console.WriteLine("Creating job...");
-				FineTuningJob job = client.CreateJob(file.Id, "gpt-3.5-turbo");	
+            OpenAIFileInfo file = fileClient.UploadFile(path, "fine-tune");
+
+            try
+            {
+
+                Console.WriteLine("Creating job...");
+                FineTuningJob job = client.CreateJob(file.Id, "gpt-3.5-turbo");	
                 Assert.True(job.Status.InProgress());
-				Console.WriteLine("Job created successfully! Cancelling...");
-				job = client.CancelJob(job.Id);
-				Assert.AreEqual(FineTuningJobStatus.Cancelled, job.Status);
+                Console.WriteLine("Job created successfully! Cancelling...");
+                job = client.CancelJob(job.Id);
+                Assert.AreEqual(FineTuningJobStatus.Cancelled, job.Status);
                 Assert.AreEqual("fine_tuning.job", job.Object);
                 Assert.False(job.Status.InProgress());
-			}
-			finally
-			{
-				fileClient.DeleteFile(file.Id);
-			}
-		}
+            }
+            finally
+            {
+                fileClient.DeleteFile(file.Id);
+            }
+        }
 
-		[Test]
-		public async Task CreateAndCancelJobAsync()
+        [Test]
+        public async Task CreateAndCancelJobAsync()
         {
             var client = new FineTuningClient();
 
@@ -81,5 +87,5 @@ namespace OpenAI.Tests.FineTuning
                 await fileClient.DeleteFileAsync(file.Id);
             }
         }
-	}
+    }
 }
